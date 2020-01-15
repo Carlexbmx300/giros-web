9612c1ff62bdab1d89d99c4d50c84fdc66f11c0d
carlos <carlosaguilarangulo@gmail.com>
2020-01-14 22:16:11 -0400

    agregamos vista de caracterisiticas

---
+++ src/components/Caracteristicas/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <mdb-navbar expand="large" position="top" transparent scrolling>
+    <mdb-navbar-brand href="#">
+      <img src="images/logoweb.png" alt="">
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <div class="menu navbar-nav">
+        <mdb-nav-item href="#">Inicio</mdb-nav-item>
+        <mdb-nav-item href="#">Giros Express</mdb-nav-item>
+        <mdb-nav-item href="#">Invita y Gana</mdb-nav-item>
+         <mdb-nav-item href="#">Blog</mdb-nav-item>
+
+      </div>
+      <div class="menu2 navbar-nav">
+         <mdb-nav-item href="#"> <img src="images/iconoMenu1.png" class="icono" alt=""> Recarga tu Telefono</mdb-nav-item>
+          <mdb-nav-item href="#" class="linea"><img src="images/iconoMenu2.png" class="icono2" alt="">Registrarse</mdb-nav-item>
+           <mdb-nav-item href="#" class="linea"><img src="images/iconoMenu3.png" class="icono3" alt="">Iniciar Sesion</mdb-nav-item>
+           <!--anadido comentario anadimos mas-->
+       </div>
+          
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+
+<script>
+  import { mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbContainer, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbIcon } from 'mdbvue';
+  export default {
+    name: 'Header',
+    components: {
+      mdbNavbar,
+      mdbNavbarBrand,
+      mdbNavbarToggler,
+      mdbNavbarNav,
+      mdbNavItem,
+      mdbContainer,
+      mdbDropdown,
+      mdbDropdownToggle,
+      mdbDropdownMenu,
+      mdbDropdownItem,
+       mdbIcon
+    }
+  }
+  
+</script>
---
+++ src/views/Caracteristicas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="caracteristicas">
+      <Header/>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+import Header from '@/components/Caracteristicas/Header.vue'
+
+export default {
+  name: 'caracteristicas',
+  components: {
+    Header,
+  
+
+  }
+}
+</script>
