5f2efa5c3dddaa3ffbda69a4034cc73d79f19711
carlos <carlosaguilarangulo@gmail.com>
2020-01-14 22:16:59 -0400

    ahora si agregamos la vista de caracteristicas

---
+++ src/components/Caracteristicas/Contenido1.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="cc1">
+        <mdb-container >
+            <mdb-row>
+                <mdb-col xl="12">
+                    <div class="mt-xl-5  float-xl-right">
+                    <h1>CARACTERISTICAS A LA MEDIDA</h1>
+                    </div>
+                </mdb-col>
+            </mdb-row>
+            <mdb-row>
+                <mdb-col xl="12"> 
+                    <div class="float-xl-right float-lg-right ">
+                   
+                     <h5>Conocemos sus necesidades y por ello hemos realizado una plataforma que le permitira tener el contro y AHORRAR TIEMPO</h5>
+                       
+                    </div>
+                </mdb-col>
+            </mdb-row>
+        </mdb-container>
+
+    </div>
+</template>
+
+<script>
+  import { mdbInput,mdbContainer, mdbRow, mdbCol,mdbBtn,mdbSelect,mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbIcon  } from 'mdbvue';
+  export default {
+    name: "Input",
+    components: {
+      mdbInput,
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbBtn,
+      mdbSelect,
+      mdbDropdown, 
+      mdbDropdownToggle, 
+      mdbDropdownMenu, 
+      mdbDropdownItem,
+        mdbIcon 
+    },
+    data() {
+      return {
+        value: ''
+        
+      };
+    }
+  }
+</script>
--- src/views/Caracteristicas.vue
+++ src/views/Caracteristicas.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="caracteristicas">
       <Header/>
+      <Contenido1/>
+
     </div>
 </template>
 <script>
 // @ is an alias to /src
 import Header from '@/components/Caracteristicas/Header.vue'
+import Contenido1 from '@/components/Caracteristicas/Contenido1.vue'
 
 export default {
   name: 'caracteristicas',
   components: {
     Header,
+    Contenido1
   
 
   }
