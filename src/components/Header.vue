c11ca1fa59f6ee7420da330455cd28a72339c7eb
luis_diego_olivera_mendivil <oliveramendivildiego@gmail.com>
2020-03-25 21:19:23 -0400

    Signed-off-by: luis_diego_olivera_mendivil <oliveramendivildiego@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,55 +1,69 @@
 
 <template>
-  <mdb-navbar expand="large" position="top" transparent scrolling>
-    <mdb-navbar-brand href="#">
-      <img src="images/logoweb.png" alt="">
-    </mdb-navbar-brand>
-    <mdb-navbar-toggler>
-      <mdb-navbar-nav right>
-        <div class="menu navbar-nav">
+<mdb-navbar expand="large" position="top" class="justify-content-between clearmarpad">
+  <mdb-navbar-brand href="#">
+    <img src="images/logoweb.png" alt="Giros Express" class='w-50 marginlogo' />
+  </mdb-navbar-brand>
+  <mdb-navbar-toggler class='h-100'>
+    <mdb-navbar-nav class='h-100' right>
+      <div class="menu navbar-nav ">
         <mdb-nav-item :to="{name:'home'}" exact>Inicio</mdb-nav-item>
-   
-        <mdb-dropdown tag="li" class="nav-item">
-            <mdb-dropdown-toggle tag="a" navLink color="success-color-dark" slot="toggle" waves-fixed>Giros Express</mdb-dropdown-toggle>
-            <mdb-dropdown-menu>
-              <mdb-dropdown-item :to="{name:'nosotros'}">Nuestra empresa</mdb-dropdown-item>
-              <mdb-dropdown-item :to="{name:'preguntas'}">Preguntas frecuentes</mdb-dropdown-item>
-            
-            </mdb-dropdown-menu>
-          </mdb-dropdown>
+
+        <mdb-dropdown tag="li" class="nav-item align-middle">
+          <mdb-dropdown-toggle tag="a" navLink color="success-color-dark" slot="toggle" waves-fixed>Giros Express</mdb-dropdown-toggle>
+          <mdb-dropdown-menu>
+            <mdb-dropdown-item :to="{name:'nosotros'}">Nuestra empresa</mdb-dropdown-item>
+            <mdb-dropdown-item :to="{name:'preguntas'}">Preguntas frecuentes</mdb-dropdown-item>
+          </mdb-dropdown-menu>
+        </mdb-dropdown>
         <mdb-nav-item :to="{name:'invita'}">Invita y Gana</mdb-nav-item>
-         <mdb-nav-item href="#">Blog</mdb-nav-item>
+        <mdb-nav-item href="#">Blog</mdb-nav-item>
 
       </div>
-      <div class="menu2 navbar-nav">
-         <mdb-nav-item href="/recarga"> <img src="images/iconoMenu1.png" class="icono" alt=""> Recarga tu Telefono</mdb-nav-item>
-          <mdb-nav-item href="#" class="linea"><img src="images/iconoMenu2.png" class="icono2" alt="">Registrarse</mdb-nav-item>
-           <mdb-nav-item href="#" class="linea"><img src="images/iconoMenu3.png" class="icono3" alt="">Iniciar Sesion</mdb-nav-item>
-           <!--anadido comentario anadimos mas-->
-       </div>
-          
-      </mdb-navbar-nav>
-    </mdb-navbar-toggler>
-  </mdb-navbar>
+      <div class="menu2 navbar-nav d-flex justify-content-start">
+        <mdb-nav-item href="/recarga" class="adoble"><img src="images/iconoMenu3.png" class="icono" alt=""><span>Realiza un<br /> Giro</span></mdb-nav-item>
+        <mdb-nav-item href="/recarga" class="adoble"> <img src="images/iconoMenu1.png" class="icono" alt=""><span>Recarga tu <br />Telefono</span></mdb-nav-item>
+        <mdb-nav-item href="/recarga" class="asimple"><img src="images/iconoMenu2.png" class="icono d-flex justify-content-center" alt="">
+          <span span class="">Registrarse</span>
+        </mdb-nav-item>
+        <mdb-nav-item href="/recarga" class="asimple"><img src="images/iconoMenu3.png" class="icono" alt=""><span>Iniciar Sesion</span></mdb-nav-item>
+        <!--anadido comentario anadimos mas-->
+      </div>
+
+    </mdb-navbar-nav>
+  </mdb-navbar-toggler>
+</mdb-navbar>
 </template>
 
 <script>
-  import { mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbContainer, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbIcon } from 'mdbvue';
-  export default {
-    name: 'Header',
-    components: {
-      mdbNavbar,
-      mdbNavbarBrand,
-      mdbNavbarToggler,
-      mdbNavbarNav,
-      mdbNavItem,
-      mdbContainer,
-      mdbDropdown,
-      mdbDropdownToggle,
-      mdbDropdownMenu,
-      mdbDropdownItem,
-       mdbIcon
-    }
+import {
+  mdbDropdown,
+  mdbDropdownToggle,
+  mdbDropdownMenu,
+  mdbDropdownItem,
+  mdbContainer,
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem,
+  mdbIcon
+}
+from 'mdbvue';
+export default {
+  name: 'Header',
+  components: {
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem,
+    mdbContainer,
+    mdbDropdown,
+    mdbDropdownToggle,
+    mdbDropdownMenu,
+    mdbDropdownItem,
+    mdbIcon
   }
-  
+}
 </script>
